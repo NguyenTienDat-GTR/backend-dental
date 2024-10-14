830367fa38ddb307d727f7b060260a47640abf31
datgtr001 <19473791.dat@student.iuh.edu.vn>
2024-10-14 18:20:29 +0700

    create server

---
+++ server-dental/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const IP = process.env.IP_SERVER;
+
+const app = express();
+const http = require("http").createServer(app);
+
+// Cấu hình CORS
+const corsOptions = {
+    origin: "*", // Cho phép tất cả các domain. Thay đổi thành domain cụ thể nếu cần.
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Các phương thức HTTP được phép
+    allowedHeaders: ["Content-Type", "Authorization"], // Các header được phép
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the homepage!");
+});
+
+app.listen(port, () => {
+    //ket noi database
+    console.log(`Server is running on ${IP}${port}`);
+});
