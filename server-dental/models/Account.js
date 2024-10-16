67336cee2b4f01e2a0b4aaaee76ddb308a498584
NguyenTienDat-GTR <tiendat.107001@gmail.com>
2024-10-16 11:44:03 +0700

    add filed isActive into model Account

--- server-dental/models/Account.js
+++ server-dental/models/Account.js
@@ -2,35 +2,39 @@ const mongoose = require("mongoose");
 
 // Hàm để lấy thời gian theo múi giờ Việt Nam dưới dạng chuỗi
 const getVietnamTimeString = () => {
-    const now = new Date();
-    return now.toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
+  const now = new Date();
+  return now.toLocaleString("en-US", { timeZone: "Asia/Ho_Chi_Minh" });
 };
 
 const accountSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        enum: ["admin", "doctor", "employee"],
-        required: true,
-    },
-    createBy: {
-        type: String,
-        required: true,
-    },
-    createAt: {
-        type: String,  // Thay đổi từ Date sang String
-        required: true,
-        default: getVietnamTimeString, // Sử dụng chuỗi thời gian Việt Nam
-    },
-
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "doctor", "employee"],
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    require: true,
+    default: true,
+  },
+  createBy: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: String, // Thay đổi từ Date sang String
+    required: true,
+    default: getVietnamTimeString, // Sử dụng chuỗi thời gian Việt Nam
+  },
 });
 
 module.exports = mongoose.model("Account", accountSchema);
